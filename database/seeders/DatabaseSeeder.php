45da5857116d78216ff39558d6ee7e10eac69624
Anas Asiruddin <radenanas12@gmail.com>
2025-11-05 10:55:23 +0700

    login filament

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+        Admin::create([
+            'username' => 'Admin1',
+            'name' => 'Administrator 1',
+            'email' => 'admin1@example.com',
+            'password' => Hash::make('123123'),
+        ]);
+        Admin::create([
+            'username' => 'Admin2',
+            'name' => 'Administrator 2',
+            'email' => 'admin2@example.com',
+            'password' => Hash::make('123123'),
+        ]);
+        Admin::create([
+            'username' => 'Admin3',
+            'name' => 'Administrator 3',
+            'email' => 'admin3@example.com',
+            'password' => Hash::make('123123'),
+        ]);
+    }
+}
